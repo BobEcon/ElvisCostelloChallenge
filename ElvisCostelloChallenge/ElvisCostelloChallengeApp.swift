1ca369f5de27920149ff201f1dec4c9c7cff2bee
Bob Beachill <bbeachill@icloud.com>
2025-01-26 18:31:03 +0000

    Initial Commit

---
+++ ElvisCostelloChallenge/ElvisCostelloChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloChallengeApp.swift
+//  ElvisCostelloChallenge
+//
+//  Created by Robert Beachill on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
