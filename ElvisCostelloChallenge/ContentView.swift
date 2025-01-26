e2de769632eae67e0ca0666470b100907dedbde8
Bob Beachill <bbeachill@icloud.com>
2025-01-26 18:57:03 +0000

    Completed Elvis Costello Challenge

--- ElvisCostelloChallenge/ContentView.swift
+++ ElvisCostelloChallenge/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    @State private var imageName: String = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                
+                Button("Love") {
+                    message = "Love"
+                    imageName = "heart"
+                }
+                
+                Button("Understanding") {
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                }
+        
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.purple)
         }
         .padding()
     }
